2d5b97a363b0da314183a12370877165a00c6578
fikri.ramadhan <fikri.ramadhan@jogwks0222.mshome.net>
2019-11-20 11:35:04 +0700

    First

---
+++ Object Repository/Page_Track Shipment/div_Air Waybill 1                          _0dc490.rs
@@ -0,0 +1,126 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Air Waybill 1                          _0dc490</name>
+   <tag></tag>
+   <elementGuidId>ab6ab1fb-cd9a-4bea-846f-f7fd344aca94</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='queryArea']/div[2]/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>col-xs-12 col-sm-6</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                                
+                                    Air Waybill 1
+                                    
+                                        
+                                        
+                                        *
+                                    
+                                    
+                                    
+                                
+                                
+                                    Air Waybill 2
+                                    
+                                        
+                                        
+                                        *
+                                    
+                                    
+                                    
+                                
+                                
+                                    Air Waybill 3
+                                    
+                                        
+                                        
+                                        *
+                                    
+                                    
+                                    
+                                
+                                
+                                    Air Waybill 4
+                                    
+                                        
+                                        
+                                        *
+                                    
+                                    
+                                    
+                                
+                                
+                                    Air Waybill 5
+                                    
+                                        
+                                        
+                                        *
+                                    
+                                    
+                                    
+                                
+                                
+                                    
+                                    
+                                
+                            </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;queryArea&quot;)/div[@class=&quot;row input-area&quot;]/div[@class=&quot;col-xs-12 col-sm-6&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='queryArea']/div[2]/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Enter the air waybill number(s) of your shipment(s) to view its status.'])[1]/following::div[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Track Shipment'])[2]/following::div[3]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/div/div/div/div[2]/div</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/newsuit.ts
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>newsuit</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>225278a7-2bdd-44d0-a9a6-d066c288dd3d</testSuiteGuid>
+   <testCaseLink>
+      <guid>c03412a3-a6ea-48e3-9537-b4291a6385ac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/error msg</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>f0b56b4b-b3de-42d4-8c9e-bdd1a6c51545</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/csvdata</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>f0b56b4b-b3de-42d4-8c9e-bdd1a6c51545</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>data</value>
+         <variableId>2823d686-fb42-4d8e-b6aa-5496dd0ba9ed</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
